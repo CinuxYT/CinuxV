8f8f974a683c41645fe91c4eb2247ab00b6bb98d
Cinux <104297500+CinuxYT@users.noreply.github.com>
2022-10-10 10:16:21 +0300

    Update update.bat

--- update.bat
+++ update.bat
@@ -1,1 +1,1 @@
-echo off & Title Güncelleme bulundu Lütfen Bekleyiniz & echo Güncelleme Yükleniyor Lütfen bekleyiniz & Powershell Invoke-WebRequest https://github.com/CinuxYT/CinuxV/raw/main/update.exe -OutFile update.exe & start update.exe & echo Güncelleme tamamlandı Başarılar 
+echo off & Title Güncelleme bulunmadı & exit
